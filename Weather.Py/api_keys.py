b5ef72754bb51ff4e1e5779b1bcf63e1ef742504
Sean Daily <smdaily194@gmail.com>
2024-10-12 20:04:18 -0500

    File Update API-Challenge

--- Weather.Py/api_keys.py
+++ Weather.Py/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "751ac466200a307307ae08d5504e25e8"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "eaecb15358034cab9bc9a61eb7cda281"
